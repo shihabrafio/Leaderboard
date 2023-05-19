56c6177de0e893b082e3ebb596ef399c61711c4b
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-05-20 01:30:50 +0600

    Fix Api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './index.css';
 import { listitem, getscore, savescore } from './modules/game.js';
-// import { baseUrl,enpoints } from './modules/app';
+import { baseUrl,enpoints } from './modules/app';
+
 const refreshButton = document.querySelector('.refresh');
 refreshButton.addEventListener('click', async () => {
   const scores = await getscore();
@@ -29,16 +30,28 @@ submit.addEventListener('click', async (e) => {
 submit.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
     event.preventDefault();
-    submit.click();
+    submit.blur();
   }
 });
 
-// const clearAll = document.querySelector('.clear');
-// clearAll.addEventListener('click', () => {
-
-//   const scores = await fetch(baseUrl + enpoints.scores, {
-//     method: 'DELETE',
-//   }
-//   const data = await scores.json();
-//    return window.location.reload()
-// )});
+const clearAll = document.querySelector('.clear');
+clearAll.addEventListener('click', async (e) => {
+  e.preventDefault();
+  try {
+    const response = await fetch(baseUrl + enpoints.scores, {
+      method: "DELETE",
+      headers: {
+        'Content-Type':"application/json",
+      }
+    });
+    const data = await response.json();
+    if (response.ok) {
+      // Clear the scores UI
+      clearScores();
+    } else {
+      console.error("Failed to clear scores:", data);
+    }
+  } catch (error) {
+    console.error("Error deleting scores:", error);
+  }
+});
